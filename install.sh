6610d9c10e2dbbae1bbd49265fbe68f49475643e
Sergey Galkin <sergey.gals@gmail.com>
2019-08-28 01:52:32 +0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt upgrade -y
 apt install vim openssh zsh git vim
 curl -O https://github.com/sergeygalkin.keys
 cat sergeygalkin.keys >> .ssh/authorized_keys
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" || true
 termux-setup-storage
 
