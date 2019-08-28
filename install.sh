4673bf70d883bfce1a84e57d146526fbb4a5601a
Sergey Galkin <sergey.gals@gmail.com>
2019-08-28 09:55:35 +0400

    Fix in install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -xe
 apt update
 apt upgrade -y
-apt install vim openssh zsh git vim
+apt install -y vim openssh zsh git vim
 curl -O https://github.com/sergeygalkin.keys
 cat sergeygalkin.keys > .ssh/authorized_keys
 if [ ! -d .oh-my-zsh ]; then  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; fi
