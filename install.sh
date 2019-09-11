c9e8a5ef7d4ae5dcf110978a656068a0c77c34cc
Sergey Galkin <sergey.gals@gmail.com>
2019-09-11 10:59:52 +0400

    Install improvments

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ mv sergeygalkin.keys > .ssh/authorized_keys
 if [ ! -d .oh-my-zsh ]; then  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; fi
 termux-setup-storage
 chsh -s zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+OZSH_CUST_PLUG='.oh-my-zsh/custom/plugins'
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${OZSH_CUST_PLUG}/zsh-autosuggestions || (cd ${OZSH_CUST_PLUG}/zsh-autosuggestions; git pull)
+git clone https://github.com/popstas/zsh-command-time.git ${OZSH_CUST_PLUG}/command-time || (cd ${OZSH_CUSTOM_PLUG}/command-time; git pull)
 wget -c  https://raw.githubusercontent.com/sergeygalkin/termux/master/.zshrc
 exit
 
