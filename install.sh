bea84ef89147876f91e87cd31a86c6b640acd6c7
Sergey Galkin <sergey.gals@gmail.com>
2019-09-11 11:02:00 +0400

    Update install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -xe
 apt update
 apt upgrade -y
-apt install -y vim openssh zsh git vim
+apt install -y vim openssh zsh git vim wget
 curl -O https://github.com/sergeygalkin.keys
 mv sergeygalkin.keys > .ssh/authorized_keys
 if [ ! -d .oh-my-zsh ]; then  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; fi
@@ -11,6 +11,6 @@ chsh -s zsh
 OZSH_CUST_PLUG='.oh-my-zsh/custom/plugins'
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${OZSH_CUST_PLUG}/zsh-autosuggestions || (cd ${OZSH_CUST_PLUG}/zsh-autosuggestions; git pull)
 git clone https://github.com/popstas/zsh-command-time.git ${OZSH_CUST_PLUG}/command-time || (cd ${OZSH_CUSTOM_PLUG}/command-time; git pull)
-wget -c  https://raw.githubusercontent.com/sergeygalkin/termux/master/.zshrc
+curl -O  https://raw.githubusercontent.com/sergeygalkin/termux/master/.zshrc
 exit
 
