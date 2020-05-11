bc84bb5853f1c5487d021e6d88739189d8b9842b
Sergey Galkin <sergey.gals@gmail.com>
2020-05-11 11:21:42 +0400

    Small fixes in install.sh

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Copy me
 ```
+pkg add curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/sergeygalkin/termux/master/install.sh)"
 ```
--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 set -xe
 apt update
 apt upgrade -y
-apt install -y vim openssh zsh git vim wget
+apt install -y vim openssh zsh git vim wget gnupg
 curl -O https://github.com/sergeygalkin.keys
-mv sergeygalkin.keys > .ssh/authorized_keys
+cat sergeygalkin.keys > .ssh/authorized_keys
 if [ ! -d .oh-my-zsh ]; then  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"; fi
 termux-setup-storage
 chsh -s zsh
@@ -12,5 +12,6 @@ OZSH_CUST_PLUG='.oh-my-zsh/custom/plugins'
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${OZSH_CUST_PLUG}/zsh-autosuggestions || (cd ${OZSH_CUST_PLUG}/zsh-autosuggestions; git pull)
 git clone https://github.com/popstas/zsh-command-time.git ${OZSH_CUST_PLUG}/command-time || (cd ${OZSH_CUSTOM_PLUG}/command-time; git pull)
 curl -O  https://raw.githubusercontent.com/sergeygalkin/termux/master/.zshrc
+
 exit
 
